e29b16349b5682ecc2bb32de7e3b62d57fd7c6a3
Halevy, Roey <Roey.Halevy@ncr.com>
2017-07-17 07:46:36 +0300

    added sa password

--- build.ps1
+++ build.ps1
@@ -4,4 +4,4 @@ Set-Location .\iis-docker\windowsservercore
 docker image build . -t windowscore-iis-mssqlexpress
 
 #docker run -d --name container_name image_name
-docker run -d -p 1433:1433 -e sa_password=<SA_PASSWORD> -e ACCEPT_EULA=Y --name application_server windowscore-iis-mssqlexpress
+docker run -d -p 1433:1433 -e sa_password=Qwe12345 -e ACCEPT_EULA=Y --name application_server windowscore-iis-mssqlexpress
